63b862238ee936308cf4974ad9fc9d6219e7452b
masterKIE <prawit.na59@snru.ac.th>
2019-03-18 14:34:11 +0700

    test

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//StringType ตัวแปรที่เป็นอักษร
+
+
+
+var myName = "Doramon"//นี่คือการประกาศ ที่อาศัยชนิดของ Value บอก Datatype
+var mySurname:String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+
+
+
+
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//ExplicitVariable
+//การประกาศตัวแปรนะจ้ะ
+
+var myName = "prawitKie"
+myName = "Doramcn"
+
+//ExplicitConstant
+let myFriend = "Nopita"
+//myFriend = "Sunako"Error เพราะ myFriend เป็น คำคงที่
+
+
+
